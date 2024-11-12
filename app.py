e951a1da631c4cd48da2ee8d53e7f5aa1d1340a1
maxhilding <maxi.hilding@gmail.com>
2024-11-11 23:52:14 -0500

    CR of CRUD done

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 # Import libraries
-
+from flask import Flask, request, url_for, redirect, render_template
 # Instantiate Flask functionality
-
+app = Flask(__name__)
 # Sample data
-
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 # Read operation
-
+@app.route('/')
+def get_transactions():
+    return render_template('transactions.html', transactions=transactions)
 # Create operation
-
+@app.route('/add', methods=["GET", "POST"])
+def add_transaction():
+    if request.method = "POST":
+        transaction = {
+            'id': len(transactions) + 1,            # Generate a new ID based on the current length of the transactions list
+            'date': request.form['date'],           # Get the 'date' field value from the form
+            'amount': float(request.form['amount']) # Get the 'amount' field value from the form and convert it to a float
+        }
+        transactions.append(transaction)
+        return redirect(url_for("get_transacions"))
+    return render_template('form.html')
 # Update operation
 
 # Delete operation
